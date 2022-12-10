9c796e6cd5121925a7659ac18327ca343115bc18
vivianassis <vivianassis11@gmail.com>
2022-12-10 17:25:38 -0300

    atualização_projeto_alura_java-oo-static

--- projeto-encapsulamento-pt1/src/TesteGetSet.java
+++ projeto-encapsulamento-pt1/src/TesteGetSet.java
@@ -1,8 +1,10 @@
 public class TesteGetSet {
     public static void main(String[] args) {
         Conta conta = new Conta(1337, 45678-2);
-        //conta.numero = 1337;
-        //conta.setNumero(1337);
+        Conta conta1 = new Conta(1576, 4567-9); //testando novo método de contagem de contas
+        Conta conta2 = new Conta(3254, 6859-0); //usando total++ no construtor junto com a variável total static na classe
+        //conta.numero = 1337;   ((não precisa mais))
+        //conta.setNumero(1337);  ((pois ja passamos os parâmetros no próprio construtor)
         System.out.println(conta.getNumero());
 
         conta.deposita(300);
@@ -25,8 +27,8 @@ public class TesteGetSet {
         //é o mesmo
 
             //System.out.println(titularDaConta);
-        System.out.println(matheus);
-        System.out.println(conta.getTitular());
+          //System.out.println(matheus);
+         //System.out.println(conta.getTitular());
         //Todos dão no mesmo resultado, pois são a mesma coisa, só inicializamos um cliente.
 
     }
