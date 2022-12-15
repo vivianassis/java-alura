1eed9bfaace19226a6c7f13daad796e9c95c6cc6
vivianassis <vivianassis11@gmail.com>
2022-12-15 06:05:40 -0300

    polimorfismo-pt2-contas

---
+++ herdado-conta/src/defaults/TesteContas.java
@@ -0,0 +1,35 @@
+package defaults;
+
+public class TesteContas {
+
+    public static void main(String[] args) {
+
+        Conta conta = new Conta(1337, 23334);
+
+        conta.deposita(200.0);
+
+        System.out.println(conta.getSaldo());
+
+        conta.setAgencia(570);
+
+        System.out.println(conta.getAgencia());
+
+        System.out.println("o total de contas é : " + Conta.getTotal());
+
+
+        ContaCorrente contaCorrente = new ContaCorrente(111,111);
+        ContaPoupanca contaPoupanca = new ContaPoupanca(222,222);
+
+        contaCorrente.deposita(100);
+        System.out.println(contaCorrente.getSaldo());
+        contaPoupanca.deposita(200);
+        System.out.println(contaPoupanca.getSaldo());
+        contaCorrente.transfere(100, contaPoupanca); //apesar de pedir um parametro 'Conta', graças ao uso do polimorfismo, podemos colocar algum parâmetro mais específico como uma conta-corrente ou poupança aqui que irá funcionar.
+        System.out.println(contaCorrente.getSaldo());
+
+        contaCorrente.saca(90);
+        System.out.println(contaCorrente.getSaldo());
+        }
+
+
+}
